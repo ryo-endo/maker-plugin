5772ccb7573e13c66981ca648440b46d7872a3ca
Kiyoshi Yamamura <kiyoshi.yamamura@cule.jp>
2016-02-17 09:28:10 +0900

    メーカー管理プラグインの修正

---
+++ Migration/Version201507231300.php
@@ -0,0 +1,163 @@
+<?php
+/*
+* This file is part of EC-CUBE
+*
+* Copyright(c) 2000-2015 LOCKON CO.,LTD. All Rights Reserved.
+* http://www.lockon.co.jp/
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version201507231300 extends AbstractMigration
+{
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->createPlgMakerPlugin($schema);
+        $this->createPlgMaker($schema);
+        $this->createPlgProductMaker($schema);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $schema->dropTable('plg_maker_plugin');
+        $schema->dropTable('plg_maker');
+        $schema->dropTable('plg_product_maker');
+    }
+
+    protected function createPlgMakerPlugin(Schema $schema)
+    {
+        $table = $schema->createTable("plg_maker_plugin");
+        $table->addColumn('plugin_id', 'integer', array(
+            'autoincrement' => true,
+        ));
+
+        $table->addColumn('plugin_code', 'text', array(
+            'notnull' => true,
+        ));
+
+        $table->addColumn('plugin_name', 'text', array(
+            'notnull' => true,
+        ));
+
+        $table->addColumn('sub_data', 'text', array(
+            'notnull' => false,
+        ));
+
+        $table->addColumn('auto_update_flg', 'smallint', array(
+            'notnull' => true,
+            'unsigned' => false,
+            'default' => 0,
+        ));
+
+        $table->addColumn('del_flg', 'smallint', array(
+            'notnull' => true,
+            'unsigned' => false,
+            'default' => 0,
+        ));
+
+        $table->addColumn('create_date', 'datetime', array(
+            'notnull' => true,
+            'unsigned' => false,
+        ));
+
+        $table->addColumn('update_date', 'datetime', array(
+            'notnull' => true,
+            'unsigned' => false,
+        ));
+
+        $table->setPrimaryKey(array('plugin_id'));
+    }
+
+    protected function createPlgMaker(Schema $schema)
+    {
+        $table = $schema->createTable("plg_maker");
+        $table->addColumn('maker_id', 'integer', array(
+            'autoincrement' => true,
+        ));
+
+        $table->addColumn('name', 'text', array(
+            'notnull' => true,
+        ));
+
+        $table->addColumn('rank', 'integer', array(
+            'notnull' => true,
+            'unsigned' => false,
+            'default' => 0,
+        ));
+
+        $table->addColumn('del_flg', 'smallint', array(
+            'notnull' => true,
+            'unsigned' => false,
+            'default' => 0,
+        ));
+
+        $table->addColumn('create_date', 'datetime', array(
+            'notnull' => true,
+            'unsigned' => false,
+        ));
+
+        $table->addColumn('update_date', 'datetime', array(
+            'notnull' => true,
+            'unsigned' => false,
+        ));
+
+        $table->setPrimaryKey(array('maker_id'));
+    }
+
+    protected function createPlgProductMaker(Schema $schema)
+    {
+        $table = $schema->createTable("plg_product_maker");
+        $table->addColumn('product_id', 'integer', array(
+            'notnull' => true,
+        ));
+
+        $table->addColumn('maker_id', 'text', array(
+            'notnull' => true,
+        ));
+
+        $table->addColumn('maker_url', 'text', array());
+
+        $table->addColumn('del_flg', 'smallint', array(
+            'notnull' => true,
+            'unsigned' => false,
+            'default' => 0,
+        ));
+
+        $table->addColumn('create_date', 'datetime', array(
+            'notnull' => true,
+            'unsigned' => false,
+        ));
+
+        $table->addColumn('update_date', 'datetime', array(
+            'notnull' => true,
+            'unsigned' => false,
+        ));
+
+        $table->setPrimaryKey(array('product_id'));
+    }
+
+    function getMakerCode()
+    {
+        $config = \Eccube\Application::alias('config');
+
+        return "";
+    }
+}
---
+++ Migration/Version201511201300.php
@@ -0,0 +1,43 @@
+<?php
+/*
+* This file is part of EC-CUBE
+*
+* Copyright(c) 2000-2015 LOCKON CO.,LTD. All Rights Reserved.
+* http://www.lockon.co.jp/
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version201511201300 extends AbstractMigration
+{
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $table = $schema->getTable('plg_product_maker');
+        if (!$table->hasColumn('maker_url')) {
+            $table->addColumn('maker_url', 'text', array('notnull' => true));
+        }
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+    }
+
+}

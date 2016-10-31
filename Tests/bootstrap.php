b30df9d91eedabfdca52a026f7169b8c29a05d21
lqdung <lqdung@lockon-vn.com>
2016-10-31 15:16:26 +0700

    [FIX] Fix code style.

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -7,5 +7,5 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-$loader = require __DIR__ . '/../../../../autoload.php';
+$loader = require __DIR__.'/../../../../autoload.php';
 $loader->add('Plugin\Maker\Tests', __DIR__);

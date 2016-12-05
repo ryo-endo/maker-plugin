d759e2043cabbe36fe5cdd22213e60f135f5368f
lqdung <lqdung@lockon-vn.com>
2016-12-05 14:39:24 +0700

    [FIX] Fix unique upgrade.

---
+++ Resource/doctrine/migration/Version201612051300.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * This file is part of the Maker plugin
+ *
+ * Copyright (C) 2016 LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version201612051300 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $table = $schema->getTable('plg_product_maker');
+        if ($table->hasIndex('UNIQ_F3A9167968DA5EC3')) {
+            $table->dropIndex('UNIQ_F3A9167968DA5EC3');
+        }
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+    }
+}
